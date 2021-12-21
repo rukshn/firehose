02f81f4286154836bcdd07e7d8d00af184f9d14c
Rukshan <arkruka@gmail.com>
2021-12-21 14:55:11 +0530

    prettier code

--- src/4.0.1/types/time.ts
+++ src/4.0.1/types/time.ts
@@ -1,21 +1,24 @@
 class time {
-  private time: string
+  private time: string;
   constructor(time: string) {
-    const regex = new RegExp(/([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?/, 'gi')
+    const regex = new RegExp(
+      /([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?/,
+      "gi"
+    );
     if (regex.test(time)) {
-      this.time = time
+      this.time = time;
     }
   }
 
   getValue() {
-    return this.time
+    return this.time;
   }
 
   getJSON() {
     return {
-      time: this.time
-    }
+      time: this.time,
+    };
   }
 }
 
-export { time }
+export { time };
