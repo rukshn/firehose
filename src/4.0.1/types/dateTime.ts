9448894ef979cb9d243e7b02c8e2cdc0aa74e3c3
Rukshan <arkruka@gmail.com>
2021-12-24 21:51:25 +0530

    changed getJSON to toJSON for unification

--- src/4.0.1/types/dateTime.ts
+++ src/4.0.1/types/dateTime.ts
@@ -1,21 +1,24 @@
 class dateTime {
-  private dateTime
-  constructor (dateTime: string) {
-    const regex = new RegExp(/([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?/, 'gi')
+  private dateTime;
+  constructor(dateTime: string) {
+    const regex = new RegExp(
+      /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?/,
+      "gi"
+    );
     if (regex.test(dateTime)) {
-      this.dateTime = dateTime
+      this.dateTime = dateTime;
     }
   }
 
   getValue() {
-    return this.dateTime
+    return this.dateTime;
   }
 
-  getJSON() {
+  toJSON() {
     return {
-      dateTime: this.dateTime
-    }
+      dateTime: this.dateTime,
+    };
   }
 }
 
-export { dateTime }
+export { dateTime };
