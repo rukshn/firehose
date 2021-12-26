533ed22a2ad79eb3be01b8cf09e5ec7c28048cb4
Rukshan <arkruka@gmail.com>
2021-12-26 11:54:55 +0530

    contactDetail elemenet initial commit

---
+++ src/4.0.1/elements/contactDetail.ts
@@ -0,0 +1,70 @@
+import { ContactPoint } from "./contactPoint";
+import { Extension } from "./extension";
+
+interface ContactDetail {
+  id: string;
+  name: string;
+  telecom: Array<ContactPoint>;
+  extension: Array<Extension>;
+}
+
+class ContactDetail {
+  private _data: {
+    id: string;
+    name: string;
+    telecom: Array<ContactPoint>;
+    extension: Array<Extension>;
+  };
+  constructor(options?) {
+    Object.defineProperty(this, "_data", { value: {} });
+
+    Object.defineProperty(this, "id", {
+      enumerable: true,
+      get: () => this._data.id,
+      set: (value: string) => {
+        if (!value) return;
+        this._data.id = value;
+      },
+    });
+
+    Object.defineProperty(this, "name", {
+      enumerable: true,
+      get: () => this._data.name,
+      set: (value: string) => {
+        if (!value) return;
+        this._data.name = value;
+      },
+    });
+
+    Object.defineProperty(this, "telecom", {
+      enumerable: true,
+      get: () => this._data.telecom,
+      set: (value: Array<ContactPoint>) => {
+        if (!value) return;
+        this._data.telecom = value;
+      },
+    });
+
+    Object.defineProperty(this, "extension", {
+      enumerable: true,
+      get: () => this._data.extension,
+      set: (value: Array<Extension>) => {
+        if (!value) return;
+        this._data.extension = value;
+      },
+    });
+  }
+
+  getResourceType(): string {
+    return "contactDetail";
+  }
+
+  toJSON(): object {
+    return {
+      id: this.id,
+      name: this.name,
+      telecom: this.telecom && this.telecom.map((t) => t.toJSON()),
+      extension: this.extension && this.extension.map((e) => e.toJSON()),
+    };
+  }
+}
